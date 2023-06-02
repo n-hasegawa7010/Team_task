0ddb6437b7bc887db80dce58a23de39275da9d17
USENGROUP_n-hasegawa <n-hasegawa@usen-next.jp>
2023-06-02 16:49:37 +0900

    floatを行った。

--- pokeAPI/pokemonAPI.php
+++ pokeAPI/pokemonAPI.php
@@ -14,28 +14,43 @@
     <h1>ポケモン図鑑</h1>
 </header>
 <?php
-/** PokeAPI のデータを取得する(id=1から10のポケモンのデータ) */
-$url = 'https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0';
-$response = file_get_contents($url);
-// レスポンスデータは JSON 形式なので、デコードして連想配列にする
-$data = json_decode($response, true);
 
 // 取得結果をループさせてポケモンの名前を表示する
 function view_poke(){
+    /** PokeAPI のデータを取得する(id=1から10のポケモンのデータ) */
+    $url = 'https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0';
+    $response = file_get_contents($url);
+    // レスポンスデータは JSON 形式なので、デコードして連想配列にする
+    $data = json_decode($response, true);
+
+    
     foreach($data['results'] as $key => $value){
-        echo '<div class="poke_data">';
-        echo "<br>";
         $response_detail = file_get_contents($value['url']);
         $data_detail = json_decode($response_detail, true);
-        echo "<img src={$data_detail['sprites']['front_default']} alt='ポケモン画像'"."<br>"; // デフォルト正面
-        echo "<br>";
-        echo $value['name']."<br>";  // 名前
-        // var_dump($data['type']); // タイプ
-        echo "たかさ：".$data_detail['height']." m<br>"; // たかさ
-        echo "おもさ：".$data_detail['weight']." kg<br>"; // おもさ
-        echo "<div>";
+
+        echo '<div class = "poke_data">';
+            echo "<br>";
+            echo '<div class = "poke_img">';
+                echo "<img src={$data_detail['sprites']['front_default']} alt='ポケモン画像'"."<br>"; // デフォルト正面
+                echo "<br>";
+            echo '</div>';
+
+            echo '<div class = "poke_ex">';
+                // 名前
+                echo $value['name']."<br>";
+                
+                // var_dump($data['type']); // タイプ
+                
+                // たかさ
+                echo "たかさ：".$data_detail['height']." m<br>";
+                
+                // おもさ
+                echo "おもさ：".$data_detail['weight']." kg<br>";
+            echo '</div>';
+        echo '</div>';
     }
 }
+view_poke();
 ?>
 
 </body>
