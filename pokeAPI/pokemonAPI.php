0afef7e14848bbe5598052a506286af65b905df8
USENGROUP_n-hasegawa <n-hasegawa@usen-next.jp>
2023-06-02 16:01:27 +0900

    pokemon.css作成。

--- pokeAPI/pokemonAPI.php
+++ pokeAPI/pokemonAPI.php
@@ -28,5 +28,6 @@ foreach($data['results'] as $key => $value){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/pokemonAPI.css">
     <title>PokemonAPI_Test</title>
 </head>
