29999fc7e1cff84fabcd382dcd3fcbfbf6212460
USENGROUP_n-hasegawa <n-hasegawa@usen-next.jp>
2023-06-05 13:04:43 +0900

    test.phpでタイプの参照方法を確認。

--- pokeAPI/test.php
+++ pokeAPI/test.php
@@ -1,11 +1,33 @@
 <?php
 /** PokeAPI のデータを取得する(id=1から10のポケモンのデータ) */
-$url = 'https://pokeapi.co/api/v2/pokemon/?limit=2&offset=0';
+// $url = 'https://pokeapi.co/api/v2/pokemon/?limit=2&offset=0';
+// $response = file_get_contents($url);
+// // レスポンスデータは JSON 形式なので、デコードして連想配列にする
+// $data = json_decode($response, true);
+// echo "ポケモンのデータ"."<br>";
+// print("<pre>");
+// var_dump($data);
+// print("</pre>");
+// echo "<hr>";
+
+/** PokeAPI のデータを取得する(URL末尾の数字はポケモン図鑑のID) */
+$url = 'https://pokeapi.co/api/v2/pokemon/1/';
 $response = file_get_contents($url);
 // レスポンスデータは JSON 形式なので、デコードして連想配列にする
 $data = json_decode($response, true);
-echo "ポケモンのデータ"."<br>";
+
+// print("<pre>");
+// var_dump($data);
+// print("</pre>");
+
 print("<pre>");
-var_dump($data);
-print("</pre>");
+var_dump($data['name']); // 名前
 echo "<hr>";
+var_dump($data['types']['0']['type']['name']);
+var_dump($data['types']['1']['type']['name']);
+// var_dump($data['sprites']['front_default']); // 正面向きのイメージ
+// var_dump($data['height']); // たかさ
+// var_dump($data['weight']); // おもさ
+print("</pre>");
+
+?>
